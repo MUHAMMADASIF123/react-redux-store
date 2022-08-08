816a131b9c3e267b8367c6567786373e23883cd6
MUHAMMADASIF123 <52148492+MUHAMMADASIF123@users.noreply.github.com>
2022-08-08 23:23:09 +0500

    this is header commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Header from './containers/Header'
+// import Header from './containers/Header'
+// import Header from './containers/Header'
 import Productlisting from './containers/Productlisting';
 import { Routes, Route  } from 'react-router-dom';
 import ProductDetails from './containers/ProductDetails'
 function App() {
   return (
     <div className="App ">
-   <Header/>
+   {/* <Header/> */}
 <div className='container'>
      <Routes>
      <Route path='/' element={<Productlisting/>}></Route>
